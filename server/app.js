809dec88d63e620fc97bbf5b3ca70418b8839164
ddbonilla <ddbonillav@gmail.com>
2023-04-05 13:05:52 -0700

    CRD completed, missing U

--- server/app.js
+++ server/app.js
@@ -11,7 +11,10 @@ app.use(express.json());
 //controllers
 const {
     getAllItems,
+    getOneItem,
+    addItem,
     getAllManagers,
+    deleteItem,
  
 } = require('./db/controllers');
 
@@ -20,6 +23,16 @@ app.get('/', (req, res) => res.send('Welcome to SpareWars Bowling Inventory!'))
 
 app.get('/inventory', (req, res) => {
     getAllItems()
+     .then(data =>  {
+         res.send(data)
+     })
+     .catch((err) => {
+         res.send(err);
+     })   
+ })
+
+app.get('/inventory/:item', (req, res) => {
+    getOneItem(req.params.item)
      .then(data =>  {
          res.send(data)
      })
@@ -39,7 +52,30 @@ app.get('/managers', (req, res) => {
  })
 
 //POST Requests
+app.post('/inventory', (req, res) => {
+    if(req.body) {
+        addItem(req.body)
+        .then(data =>  {
+            res.send(data)
+        })
+        .catch((err) => {
+            res.send(err);
+        })   
+    } else {
+     res.status(400).send('invalid request');
+    }
+ })
+
 //PATCH/PUT Requests
 //DELETE Requests
+app.delete('/inventory/:id', (req, res) => {
+    deleteItem(req.params.id)
+    .then(() => {
+        res.send('item deleted')
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+})
 
 module.exports = app;
--- server/db/controllers.js
+++ server/db/controllers.js
@@ -1,13 +1,32 @@
 const knex = require('./dbConnection');
 
+//bowling_table queries
 const getAllItems = async () => {
     return knex.select('*').from('bowling_tbl')
 }
+
+const getOneItem = async (item) => {
+    return knex.select('*').from('bowling_tbl').where('ItemName', 'ilike', `%${item}%`);
+}
+
+const addItem = async (item) => {
+    return knex('bowling_tbl').insert(item);
+}
+
+const deleteItem = async (id) => {
+    return knex('bowling_tbl').where({InvId: id}).delete();
+}
+
+
+//users_table queries
 const getAllManagers = async () => {
     return knex.select('*').from('users_tbl')
 }
 
 module.exports = {
     getAllItems,
+    getOneItem,
+    addItem,
+    deleteItem,
     getAllManagers
 };
